d71dff06760edaf271ba67a49f629755482158aa
DerekStride <djgstride@gmail.com>
2017-06-29 21:58:00 -0400

    Working on getting react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cookbooks
+
   post '/graphql', to: 'graphql#create'
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: '/graphql' if Rails.env.development?
 end
